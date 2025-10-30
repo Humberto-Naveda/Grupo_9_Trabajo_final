fe32d417de5e765c8c19d71a3d58bce3956f36ac
saul roldan <saul28roldan@gmail.com>
2025-10-30 12:45:49 -0300

    base

--- archivos/gp9_cinemacentro_basededatos.sql
+++ archivos/gp9_cinemacentro_basededatos.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 24-10-2025 a las 01:09:55
+-- Tiempo de generación: 30-10-2025 a las 16:44:56
 -- Versión del servidor: 10.4.32-MariaDB
 -- Versión de PHP: 8.2.12
 
@@ -28,211 +28,48 @@ USE `gp9_cinemacentro_basededatos`;
 --
 -- Estructura de tabla para la tabla `asiento`
 --
-
-CREATE TABLE `asiento` (
-  `idAsiento` int(11) NOT NULL,
-  `fila` int(11) NOT NULL,
-  `numero` int(11) NOT NULL,
-  `estado` int(11) NOT NULL,
-  `idFuncion` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+-- Error leyendo la estructura de la tabla gp9_cinemacentro_basededatos.asiento: #1932 - Table &#039;gp9_cinemacentro_basededatos.asiento&#039; doesn&#039;t exist in engine
+-- Error leyendo datos de la tabla gp9_cinemacentro_basededatos.asiento: #1064 - Algo está equivocado en su sintax cerca &#039;FROM `gp9_cinemacentro_basededatos`.`asiento`&#039; en la linea 1
 
 -- --------------------------------------------------------
 
 --
 -- Estructura de tabla para la tabla `comprador`
 --
-
-CREATE TABLE `comprador` (
-  `id_comprador` int(11) NOT NULL,
-  `dni` int(11) NOT NULL,
-  `nombre` varchar(60) NOT NULL,
-  `fechaNac` date NOT NULL,
-  `password` int(11) NOT NULL,
-  `medioPago` varchar(120) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Volcado de datos para la tabla `comprador`
---
-
-INSERT INTO `comprador` (`id_comprador`, `dni`, `nombre`, `fechaNac`, `password`, `medioPago`) VALUES
-(1, 44221133, 'Juan Zarate', '1995-05-15', 1234, 'Tarjeta Debito');
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `detalle_ticket`
---
-
-CREATE TABLE `detalle_ticket` (
-  `idDetalle` int(11) NOT NULL,
-  `idTicket` int(11) NOT NULL,
-  `idFuncion` int(11) NOT NULL,
-  `cantidad` int(11) NOT NULL,
-  `subtotal` double NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+-- Error leyendo la estructura de la tabla gp9_cinemacentro_basededatos.comprador: #1932 - Table &#039;gp9_cinemacentro_basededatos.comprador&#039; doesn&#039;t exist in engine
+-- Error leyendo datos de la tabla gp9_cinemacentro_basededatos.comprador: #1064 - Algo está equivocado en su sintax cerca &#039;FROM `gp9_cinemacentro_basededatos`.`comprador`&#039; en la linea 1
 
 -- --------------------------------------------------------
 
 --
 -- Estructura de tabla para la tabla `funcion`
 --
-
-CREATE TABLE `funcion` (
-  `idFuncion` int(11) NOT NULL,
-  `idPelicula` int(11) NOT NULL,
-  `idioma` varchar(30) NOT NULL,
-  `es3D` int(11) NOT NULL,
-  `subtitulada` int(11) NOT NULL,
-  `horaInicio` time NOT NULL,
-  `horaFin` time NOT NULL,
-  `precio` double NOT NULL,
-  `idSala` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+-- Error leyendo la estructura de la tabla gp9_cinemacentro_basededatos.funcion: #1932 - Table &#039;gp9_cinemacentro_basededatos.funcion&#039; doesn&#039;t exist in engine
+-- Error leyendo datos de la tabla gp9_cinemacentro_basededatos.funcion: #1064 - Algo está equivocado en su sintax cerca &#039;FROM `gp9_cinemacentro_basededatos`.`funcion`&#039; en la linea 1
 
 -- --------------------------------------------------------
 
 --
 -- Estructura de tabla para la tabla `pelicula`
 --
-
-CREATE TABLE `pelicula` (
-  `idPelicula` int(11) NOT NULL,
-  `titulo` varchar(30) DEFAULT NULL,
-  `director` varchar(30) NOT NULL,
-  `actores` varchar(30) NOT NULL,
-  `origen` varchar(30) NOT NULL,
-  `genero` varchar(30) NOT NULL,
-  `estreno` int(11) NOT NULL,
-  `enCartelera` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+-- Error leyendo la estructura de la tabla gp9_cinemacentro_basededatos.pelicula: #1932 - Table &#039;gp9_cinemacentro_basededatos.pelicula&#039; doesn&#039;t exist in engine
+-- Error leyendo datos de la tabla gp9_cinemacentro_basededatos.pelicula: #1064 - Algo está equivocado en su sintax cerca &#039;FROM `gp9_cinemacentro_basededatos`.`pelicula`&#039; en la linea 1
 
 -- --------------------------------------------------------
 
 --
 -- Estructura de tabla para la tabla `sala`
 --
-
-CREATE TABLE `sala` (
-  `idSala` int(11) NOT NULL,
-  `nroSala` int(11) DEFAULT NULL,
-  `apta3D` int(11) NOT NULL,
-  `capacidad` int(11) NOT NULL,
-  `estado` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+-- Error leyendo la estructura de la tabla gp9_cinemacentro_basededatos.sala: #1932 - Table &#039;gp9_cinemacentro_basededatos.sala&#039; doesn&#039;t exist in engine
+-- Error leyendo datos de la tabla gp9_cinemacentro_basededatos.sala: #1064 - Algo está equivocado en su sintax cerca &#039;FROM `gp9_cinemacentro_basededatos`.`sala`&#039; en la linea 1
 
 -- --------------------------------------------------------
 
 --
 -- Estructura de tabla para la tabla `ticketcompra`
 --
-
-CREATE TABLE `ticketcompra` (
-  `idTicket` int(11) NOT NULL,
-  `fechaCompra` int(11) NOT NULL,
-  `fechaFuncion` int(11) NOT NULL,
-  `monto` int(11) NOT NULL,
-  `Id_comprador` int(11) NOT NULL,
-  `idAsiento` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Índices para tablas volcadas
---
-
---
--- Indices de la tabla `asiento`
---
-ALTER TABLE `asiento`
-  ADD PRIMARY KEY (`idAsiento`),
-  ADD KEY `idFuncion` (`idFuncion`);
-
---
--- Indices de la tabla `comprador`
---
-ALTER TABLE `comprador`
-  ADD PRIMARY KEY (`id_comprador`),
-  ADD UNIQUE KEY `dni` (`nombre`);
-
---
--- Indices de la tabla `detalle_ticket`
---
-ALTER TABLE `detalle_ticket`
-  ADD PRIMARY KEY (`idDetalle`),
-  ADD KEY `idFuncion` (`idFuncion`),
-  ADD KEY `idTicket` (`idTicket`);
-
---
--- Indices de la tabla `funcion`
---
-ALTER TABLE `funcion`
-  ADD PRIMARY KEY (`idFuncion`),
-  ADD UNIQUE KEY `idPelicula` (`idPelicula`),
-  ADD UNIQUE KEY `idSala` (`idSala`);
-
---
--- Indices de la tabla `pelicula`
---
-ALTER TABLE `pelicula`
-  ADD PRIMARY KEY (`idPelicula`),
-  ADD UNIQUE KEY `titulo` (`titulo`);
-
---
--- Indices de la tabla `sala`
---
-ALTER TABLE `sala`
-  ADD PRIMARY KEY (`idSala`),
-  ADD UNIQUE KEY `nroSala` (`nroSala`);
-
---
--- Indices de la tabla `ticketcompra`
---
-ALTER TABLE `ticketcompra`
-  ADD PRIMARY KEY (`idTicket`),
-  ADD KEY `idComprador` (`Id_comprador`),
-  ADD KEY `idAsiento` (`idAsiento`);
-
---
--- AUTO_INCREMENT de las tablas volcadas
---
-
---
--- AUTO_INCREMENT de la tabla `comprador`
---
-ALTER TABLE `comprador`
-  MODIFY `id_comprador` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
-
---
--- Restricciones para tablas volcadas
---
-
---
--- Filtros para la tabla `asiento`
---
-ALTER TABLE `asiento`
-  ADD CONSTRAINT `asiento_ibfk_1` FOREIGN KEY (`idFuncion`) REFERENCES `funcion` (`idFuncion`);
-
---
--- Filtros para la tabla `detalle_ticket`
---
-ALTER TABLE `detalle_ticket`
-  ADD CONSTRAINT `detalle_ticket_ibfk_1` FOREIGN KEY (`idFuncion`) REFERENCES `funcion` (`idFuncion`),
-  ADD CONSTRAINT `detalle_ticket_ibfk_2` FOREIGN KEY (`idTicket`) REFERENCES `ticketcompra` (`idTicket`);
-
---
--- Filtros para la tabla `funcion`
---
-ALTER TABLE `funcion`
-  ADD CONSTRAINT `funcion_ibfk_1` FOREIGN KEY (`idPelicula`) REFERENCES `pelicula` (`idPelicula`),
-  ADD CONSTRAINT `funcion_ibfk_2` FOREIGN KEY (`idSala`) REFERENCES `sala` (`idSala`);
-
---
--- Filtros para la tabla `ticketcompra`
---
-ALTER TABLE `ticketcompra`
-  ADD CONSTRAINT `fk_ticketcompra_comprador` FOREIGN KEY (`Id_comprador`) REFERENCES `comprador` (`id_comprador`),
-  ADD CONSTRAINT `ticketcompra_ibfk_2` FOREIGN KEY (`idAsiento`) REFERENCES `asiento` (`idAsiento`);
+-- Error leyendo la estructura de la tabla gp9_cinemacentro_basededatos.ticketcompra: #1932 - Table &#039;gp9_cinemacentro_basededatos.ticketcompra&#039; doesn&#039;t exist in engine
+-- Error leyendo datos de la tabla gp9_cinemacentro_basededatos.ticketcompra: #1064 - Algo está equivocado en su sintax cerca &#039;FROM `gp9_cinemacentro_basededatos`.`ticketcompra`&#039; en la linea 1
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
