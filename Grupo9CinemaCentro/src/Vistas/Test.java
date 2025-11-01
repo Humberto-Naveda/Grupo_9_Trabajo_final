20481fb6dbbd6b6e7cfd2a506861a139467a5022
saul roldan <saul28roldan@gmail.com>
2025-11-01 01:11:30 -0300

    cambios

--- Grupo9CinemaCentro/src/Vistas/Test.java
+++ Grupo9CinemaCentro/src/Vistas/Test.java
@@ -17,14 +17,18 @@ public class Test {
     public static void main(String[] args) {
           Conexion conn = new Conexion("gp9_cinemacentro_basededatos","jdbc:mariadb://localhost/","root","","org.mariadb.jdbc.Driver");
           CompradorData cd = new CompradorData(conn);
-        
+        Comprador nuevo1 = new Comprador(1,33, " Zarate", 134, " Debito", LocalDate.of(1995, 10, 15));
           Comprador nuevo = new Comprador(44221133, "Juan Zarate", 1234, "Tarjeta Debito", LocalDate.of(1995, 5, 15));
-          cd.guardarComprador(nuevo);
+       //   cd.guardarComprador(nuevo);
         
-          System.out.println("Lista de compradores:");
+     
+        //cd.modificarComprador(nuevo1);
+        cd.eliminarComprador(44221133);
+       
+       /*   System.out.println("Lista de compradores:");
           for (Comprador c : cd.listarCompradores()) {
              System.out.println(c);
-          }
+          } */
          
     }
     
